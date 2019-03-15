fead68be64eb3aef8a40c5e223c9ab6583ff9848
Jun Kim <93kimhyunjun@gmail.com>
2019-03-15 16:09:56 +0900

    사칙연산 프로그램 (계산 및 출력 부분 작업)

---
+++ calculator/js_calculator.js
@@ -0,0 +1,22 @@
+var plus = function (left, right) {
+    console.log(left + right);
+};
+
+var minus = function (left, right) {
+    console.log(left - right);
+};
+
+var division = function (left, right) {
+    console.log(left / right);
+};
+
+var multiple = function (left, right) {
+    console.log(left * right);
+};
+
+function main () {
+    plus(10, 5);
+    minus(10, 5);
+    division(10, 5);
+    multiple(10, 5);
+};
